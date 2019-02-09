55ed76b85dfd9bef33530e9e133a3edb46cabd13
Nexytpowa <mrpingbuster@gmail.com>
2019-02-09 23:11:07 +0100

    test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include <iostream>
+#include <stdio.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	} break;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
